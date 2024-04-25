a631b55873814e0e1782e9e6eb9a300be945588b
ephraim7223 <basseyephraim55@gmail.com>
2024-04-25 16:53:48 +0100

    fixed protected route issue

--- SRC/routes/user/userRoute.js
+++ SRC/routes/user/userRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { deleteAllUsers, deleteSingleUser, getAllUsers, getSingleUser, updateUser } from "../../controllers/user.Controller.js";
-// import protectRoute from "../../middlewares/protectRoute.js";
+import protectRoute from "../../middlewares/protectRoute.js";
 const router = express.Router()
 
 router.get("/", getAllUsers)
-router.get("/:id", getSingleUser)
+router.get("/:id", protectRoute,getSingleUser)
 router.delete("/delete-all", deleteAllUsers)
 router.patch('/update/:id', updateUser)
 router.delete("/delete/:id", deleteSingleUser)
