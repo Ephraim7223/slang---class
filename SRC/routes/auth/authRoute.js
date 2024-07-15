a00a96f4bde9e54a008f1ed6d3797488e73fbe65
Ephraim7223 <basseyephraim55@gmail.com>
2024-07-15 23:58:50 +0100

    added admin signin function

--- SRC/routes/auth/authRoute.js
+++ SRC/routes/auth/authRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router()
-import { signUp,signIn, forgotPassword, logout } from "../../controllers/auth.Controller.js";
+import { signUp,signIn, forgotPassword, logout, adminSignIn } from "../../controllers/auth.Controller.js";
 import upload from "../../config/multer.js";
 
 router.post('/signup', upload.fields([{ name: 'profilePic', maxCount: 1 }, { name: 'coverPhoto', maxCount: 1 }]), signUp);
 router.post("/login", signIn)
 router.post("/send-otp", forgotPassword)
 router.post('/logout', logout);
+router.post('signin', adminSignIn)
 
 export default router;
